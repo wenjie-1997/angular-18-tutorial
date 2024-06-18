dc98489863304fcbe2eaa43e40bcdd6c101091dc
Wen Jie <jack_9913@hotmail.com>
2024-06-18 16:31:46 +0800

    first commit

---
+++ src/app/housing.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  url = 'http://localhost:3000/locations';
+
+  constructor() { }
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? []
+  }
+
+  async getHousingLocationById(id: Number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json();
+  }
+
+  submitApplication(firstName: string, lastname: string, email: string) {
+    console.log(firstName, lastname, email)
+  }
+}
